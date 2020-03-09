056688e9a76d0e2aa37a0f76f44885fef7d0052f
seventeen <1529068093@qq.com>
2020-03-09 20:53:04 +0800

    sql

--- doc/db/tables_xxl_job.sql
+++ doc/db/tables_xxl_job.sql
@@ -254,3 +254,6 @@ where id in (
         handle_time
     ) > 60
 )
+
+alter table XXL_JOB_QRTZ_TRIGGER_LOG rename to XXL_JOB_QRTZ_TRIGGER_LOG_20200310001;
+create table XXL_JOB_QRTZ_TRIGGER_LOG like  XXL_JOB_QRTZ_TRIGGER_LOG_20200310001;
